753447c8a71b640864095ebd00b4bd75de1f81a6
Mia Moretti <mimickal.dev@gmail.com>
2024-03-02 00:15:27 -0500

    Initial Waterworks mod setup

---
+++ README.md
@@ -0,0 +1,15 @@
+# Waterworks
+
+<a href="LICENSE.md"><img align="right" alt="AGPL-3.0 Logo"
+src="https://www.gnu.org/graphics/agplv3-155x51.png">
+</a>
+
+Waterworks is a mod that changes how water behaves in Minecraft, to make it act more
+like Terraria. It flows, accumulates when it rains, and evaporates when the sun
+shines. It adds some tools for manipulating water, like pipes, pumps, and tanks.
+
+## License
+Copyright 2023 [Mimickal](https://github.com/Mimickal)<br/>
+This code is licensed under the
+[AGPL-3.0](https://www.gnu.org/licenses/agpl-3.0-standalone.html) license.<br/>
+Basically, any modifications to this code must be made open source.
--- src/main/java/mimickal/minecraft/waterworks/Waterworks.java
+++ src/main/java/mimickal/minecraft/waterworks/Waterworks.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package mimickal.minecraft.waterworks;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.level.block.Block;
@@ -18,13 +18,14 @@ import org.slf4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod(Waterworks.MOD_NAME)
+public class Waterworks
 {
-    // Directly reference a slf4j logger
+    public static final String MOD_NAME = "waterworks";
+
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public ExampleMod()
+    public Waterworks()
     {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
@@ -47,7 +48,7 @@ public class ExampleMod
     private void enqueueIMC(final InterModEnqueueEvent event)
     {
         // Some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+        InterModComms.sendTo(Waterworks.MOD_NAME, "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
 
     private void processIMC(final InterModProcessEvent event)
